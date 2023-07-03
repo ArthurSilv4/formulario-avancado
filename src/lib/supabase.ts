6e133c83d8af880309a5ad42644abb478c8275a3
Arthur De Souza Silva <105186158+ArthurSilv4@users.noreply.github.com>
2023-07-03 16:57:37 -0300

    Update supabase.ts tirei minhas chaves

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  'https://wchyngyohctbbpattzfn.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjaHluZ3lvaGN0YmJwYXR0emZuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4ODM1MjAyNiwiZXhwIjoyMDAzOTI4MDI2fQ.KMO9og2kqgPvEgV9BxhYrfSxiid1wsqRtYtEG2QNWvY',
+  '',
+  '',
 )
